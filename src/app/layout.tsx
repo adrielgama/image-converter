c222d3e9a5e0478fff6a1f163e49fa8f54a48926
Adriel Gama <adrielgama@gmail.com>
2025-02-11 20:30:12 -0300

    feat: add seo and buymeacoffee

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,47 @@ const montserrat = Montserrat({
 export const metadata: Metadata = {
   title: 'Image Converter',
   description: 'Convert PNG images to WebP, ICO, or compress them',
+  keywords: [
+    'image converter',
+    'free image converter',
+    'png converter',
+    'PNG to WebP',
+    'PNG to ICO',
+    'compress PNG',
+    'image optimization',
+    'WebP converter',
+    'ICO converter',
+  ],
+  authors: [
+    {
+      name: 'Adriel Gama',
+      url: 'https://adrielgama.dev',
+    },
+  ],
+  openGraph: {
+    title: 'Image Converter',
+    description:
+      'Convert PNG images to WebP, ICO, or compress them with ease. A fast and simple tool for optimizing your images.',
+    url: 'https://converter.adrielgama.dev',
+    siteName: 'Image Converter',
+    images: [
+      {
+        url: 'https://converter.adrielgama.dev/og/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'Image Converter - Optimize Your Images',
+      },
+    ],
+    locale: 'pt_BR',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Image Converter',
+    description:
+      'Convert PNG images to WebP, ICO, or compress them with ease. A fast and simple tool for optimizing your images.',
+    images: ['https://imageconverter.com/twitter-image.png'],
+  },
 }
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { ImageConverter } from '@/components/image-converter'
 import { BackgroundGradientAnimation } from '@/components/ui/background-gradient-animation'
+import { Coffee } from 'lucide-react'
+import Link from 'next/link'
+import Script from 'next/script'
 
 export default function Home() {
   return (
     <BackgroundGradientAnimation>
-      {/* <main className="container mx-auto min-h-screen p-4"> */}
       <main className="absolute inset-0 z-50 container mx-auto flex min-h-screen items-center justify-center">
         <div className="mx-auto max-w-3xl space-y-6 py-8">
           <div className="space-y-2 text-center">
@@ -16,6 +18,18 @@ export default function Home() {
           <ImageConverter />
         </div>
       </main>
+      <footer className="absolute right-0 bottom-0 left-0 z-50 flex items-center justify-center p-4">
+        <Link
+          href="https://www.buymeacoffee.com/adrielgama"
+          aria-label="Buy me a coffee"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center justify-center gap-2 rounded-full bg-white/70 px-4 py-2 text-sm font-medium text-zinc-800 shadow-lg transition-colors hover:bg-[#FFCC00]/90"
+        >
+          <span>Buy me a coffee</span>
+          <Coffee size={18} />
+        </Link>
+      </footer>
     </BackgroundGradientAnimation>
   )
 }
