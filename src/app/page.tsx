274aab0f2f02ce6cb12454b584164a68dc63abf3
Adriel Gama <adrielgama@gmail.com>
2025-02-11 19:32:12 -0300

    feat: add gradient bg effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,21 @@
-import { ImageConverter } from "@/components/image-converter";
+import { ImageConverter } from '@/components/image-converter'
+import { BackgroundGradientAnimation } from '@/components/ui/background-gradient-animation'
 
 export default function Home() {
   return (
-    <main className="container mx-auto p-4 min-h-screen">
-      <div className="max-w-3xl mx-auto space-y-6 py-8">
-        <div className="text-center space-y-2">
-          <h1 className="text-3xl font-bold">Image Converter</h1>
-          <p className="text-muted-foreground">Convert PNG images to WebP, ICO, or compress them</p>
+    <BackgroundGradientAnimation>
+      {/* <main className="container mx-auto min-h-screen p-4"> */}
+      <main className="absolute inset-0 z-50 container mx-auto flex min-h-screen items-center justify-center">
+        <div className="mx-auto max-w-3xl space-y-6 py-8">
+          <div className="space-y-2 text-center">
+            <h1 className="text-3xl font-bold text-white">Image Converter</h1>
+            <p className="text-muted-foreground text-zinc-300">
+              Convert PNG images to WebP, ICO, or compress them
+            </p>
+          </div>
+          <ImageConverter />
         </div>
-        <ImageConverter />
-      </div>
-    </main>
-  );
+      </main>
+    </BackgroundGradientAnimation>
+  )
 }
